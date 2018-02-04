4ad3e13afecbbd5eaae5019501fa1fd808070bda
Brent  Ransom <sadumedusa@gmail.com>
2018-02-04 19:56:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song
   attr_accessor :name, :artist, :genres
 
-  @@count = 0
+    @@count = 0
+    @@genres = []
+    @@artist = []
 
   def initialize(name, artist, genres)
     @@count =+ 1
   end
+
+  def count
+    @@count
+  end
+  
 end
